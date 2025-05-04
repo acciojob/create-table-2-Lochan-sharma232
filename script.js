20e94ce81bb65c5e3bf2679fa25b4943bff06a62
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-04 22:25:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function createTable() {
+function createTable(data,columns) {
+	let rn=prompt("Input number of rows");
+	let cn=prompt("Input number of columns");
+	let table=document.getElementById("myTable");
+	//table.innerHTML='';
+	//create table rows
+	let tbody=table.createTbody()
+	data.forEach(item=>{
+		
+	})
     //Write your code here
   
 }
