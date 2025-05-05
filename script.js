911a0e55e634328261792e3c8c230db1979321e9
Lochan-sharma232 <lochansharma537@gmail.com>
2025-05-05 14:12:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ for(i=0;i<=rn;i++){
 	tbody.appendChild(row);
 	for(j=0;j<=cn;j++){
 		let column=document.createElement('td');
+		cell.textContent=`Row-${i} Column-${j}`;
 	    row.appendChild(column);
 	}
 }
